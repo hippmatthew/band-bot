e707007d6cda9fe2582f26db554375c866b38f0d
Matthew Hipp <74476804+hippmatthew@users.noreply.github.com>
2024-11-07 22:11:53 -0500

    Tried making a better way to connect to channels

--- band_bot.py
+++ band_bot.py
@@ -1,6 +1,7 @@
 import asyncio
 import discord
 import os
+from bot import BandBot
 from discord.ext import commands
 from dotenv import load_dotenv
 from pathlib import Path
@@ -15,69 +16,95 @@ if not DISC_TOKEN:
 
 ydl_opts = { 'format': 'bestaudio' }
 
-band_bot = commands.Bot( command_prefix = '/', intents = discord.Intents.all() )
+band_bot = BandBot(commands.Bot( command_prefix = '/', intents = discord.Intents.all() ))
 songs = SongQueue()
 
-@band_bot.event
+@band_bot.bot.event
 async def on_ready():
-  await band_bot.tree.sync()
-  print(f'Logged in as {band_bot.user}')
+  await band_bot.bot.tree.sync()
+  print(f'Logged in as {band_bot.bot.user}')
 
-@band_bot.tree.command( name = 'join', description = 'band bot joins a voice channel' )
-async def join(ctx):
-  if not ctx.author.voice:
-    await ctx.send(f'{ctx.author.voice.channel} ain\'t no bandstand!')
+@band_bot.bot.tree.command( name = 'join', description = 'band bot joins a voice channel' )
+async def join(interaction: discord.Interaction):
+  if not isinstance(interaction.user, discord.Member):
+    await interaction.response.send_message('I only perform on the big stage and this is definitely not that')
     return
 
-  channel = ctx.author.voice.channel
-  await channel.connect()
-  await ctx.send(f'I\'m performing at the {channel}')
+  if not interaction.user.voice:
+    await interaction.response.send_message('This ain\'t no bandstand!')
+    return
 
-  return ctx.voice_client
+  channel = interaction.user.voice.channel
+  if not channel: return
 
-@band_bot.tree.command( name = 'leave', description = 'band bot leaves a voice channel' )
-async def leave(ctx):
-  if not ctx.voice_client: return
+  await band_bot.connect(channel)
+  await interaction.response.send_message(f'I\'m performing at the {channel}')
 
-  await ctx.voice_client.disconnect()
-  await ctx.send("I'm taking a break")
+@band_bot.bot.tree.command( name = 'leave', description = 'band bot leaves a voice channel' )
+async def leave(interaction: discord.Interaction):
+  if not isinstance(interaction.user, discord.Member):
+    await interaction.response.send_message('I only perform on the big stage and this is definitely not that')
+    return
 
-@band_bot.tree.command( name = 'request', description = 'band bot accepts your music request' )
-async def request(ctx, *, search: str):
-  voice = ctx.voice_client
+  if not band_bot.voice_client:
+    await interaction.response.send_message("I\'m already on break! Just let me know when you want me to play again")
+    return
 
-  if not voice:
-    if not ctx.author.voice: return
-    voice = await ctx.author.voice.channel.connect()
+  await band_bot.disconnect()
+  await interaction.response.send_message("I'm taking a break")
+
+@band_bot.bot.tree.command( name = 'request', description = 'band bot accepts your music request' )
+async def request(interaction: discord.Interaction, *, search: str):
+  if not isinstance(interaction.user, discord.Member):
+    await interaction.response.send_message('I only perform on the big stage and this is definitely not that')
+    return
+
+  if not band_bot.voice_client:
+    if not interaction.user.voice:
+      await interaction.response.send_message('This ain\'t no bandstand!')
+      return
+
+    channel = interaction.user.voice.channel
+    if not channel: return
+
+    await band_bot.connect(channel)
+    if not band_bot.voice_client: return
+    await interaction.response.send_message(f'I\'m performing at the {channel}')
 
   with YoutubeDL(ydl_opts) as ydl:
     try:
       info = ydl.extract_info( search, download = False )
     except Exception:
-      await ctx.send("Not sure I know this one. Check the url you gave me or give me better search terms")
+      await interaction.response.send_message(
+        "Not sure I know this one. Check the url you gave me or give me better search terms"
+      )
       return
 
     if not info:
-      await ctx.send('Not sure I\'m understanding what you want me to play')
+      await interaction.response.send_message('Not sure I\'m understanding what you want me to play')
       return
 
     if 'entries' in info:
       for entry in info['entries']:
         url = entry['formats'][0]['url']
         songs.add(url)
-      await ctx.send(f'I\'ll play those {len(info['entries'])} jams for you')
+      await interaction.response.send_message(f'I\'ll play those {len(info['entries'])} jams for you')
     else:
       url = info['formats'][0]['url']
       songs.add(url)
-      await ctx.send(f'Sure! I\'ll play {info['title']} for you')
+      await interaction.response.send_message(f'Sure! I\'ll play {info['title']} for you')
 
-  if not voice.is_playing():
-    await play_next(ctx, voice)
+  if not band_bot.voice_client.is_playing():
+    await play_next(interaction)
 
-@band_bot.tree.command( name = 'queue', description = 'band bot displays the song queue' )
-async def queue(ctx):
-  if not ctx.voice_client:
-    await ctx.send('I\'m on break! Gimme something to play and I\'ll head to the bandstand')
+@band_bot.bot.tree.command( name = 'queue', description = 'band bot displays the song queue' )
+async def queue(interaction: discord.Interaction):
+  if not isinstance(interaction.user, discord.Member):
+    await interaction.response.send_message('I only perform on the big stage and this is definitely not that')
+    return
+
+  if not band_bot.voice_client:
+    await interaction.response.send_message('I\'m on break! Gimme something to play and I\'ll head to the bandstand')
     return
 
   num = 1
@@ -86,49 +113,57 @@ async def queue(ctx):
       info = ydl.extract_info( song, download = False)
     title = info.get('title', 'Unknown') if info else 'Unknown'
 
-    await ctx.send(f'{num}. {title}')
+    await interaction.response.send_message(f'{num}. {title}')
 
     num += 1
 
-@band_bot.tree.command( name = 'skip', description = 'band bot skips the current song in the queue' )
-async def skip(ctx):
-  voice = ctx.voice_client
-
-  if not voice:
-    await ctx.send('I\'m on break! Gimme something to play and I\'ll head to the bandstand')
+@band_bot.bot.tree.command( name = 'skip', description = 'band bot skips the current song in the queue' )
+async def skip(interaction: discord.Interaction):
+  if not isinstance(interaction.user, discord.Member):
+    await interaction.response.send_message('I only perform on the big stage and this is definitely not that')
     return
 
-  if not voice.is_playing():
-    await ctx.send('I ain\'t playing nothing right now')
+  if not band_bot.voice_client:
+    await interaction.response.send_message('I\'m on break! Gimme something to play and I\'ll head to the bandstand')
     return
 
-  await ctx.send('Alright, Alright. I\'ll see what else there is to play')
-  voice.stop()
+  if not band_bot.voice_client.is_playing():
+    await interaction.response.send_message('I ain\'t playing nothing right now')
+    return
 
-@band_bot.tree.command( name = 'artist', description = 'band bot invites a new artist to the bandstand' )
+  await interaction.response.send_message('Alright, Alright. I\'ll see what else there is to play')
+  band_bot.voice_client.stop()
+
+@band_bot.bot.tree.command( name = 'artist', description = 'band bot invites a new artist to the bandstand' )
 @commands.has_permissions( manage_nicknames = True )
-async def artist(ctx, *, new_name: str):
+async def artist(interaction: discord.Interaction, *, new_name: str):
+  if not isinstance(interaction.user, discord.Member):
+    await interaction.response.send_message('I only perform on the big stage and this is definitely not that')
+    return
+
+  if not interaction.guild: return
+
   try:
-    await ctx.guild.me.edit( nick = new_name )
-    await ctx.send(f'Welcome to the stage, {new_name}!')
+    await interaction.guild.me.edit( nick = new_name )
+    await interaction.response.send_message(f'Welcome to the stage, {new_name}!')
   except discord.Forbidden:
-    await ctx.send(f'You can\'t tell me which performer to invite')
+    await interaction.response.send_message(f'You can\'t tell me which performer to invite')
   except Exception as e:
-    await ctx.send(f'I would invite them to play, but I encountered the exception: {e}')
+    await interaction.response.send_message(f'I would invite them to play, but I encountered the exception: {e}')
 
-@band_bot.tree.command( name = 'clear', description = 'band bot clears the song queue' )
-async def clear(ctx):
-  voice = ctx.voice_client
-
-  if not voice:
-    await ctx.send('I\'m on break! Gimme something to play and I\'ll head to the bandstand')
+@band_bot.bot.tree.command( name = 'clear', description = 'band bot clears the song queue' )
+async def clear(interaction: discord.Interaction):
+  if not band_bot.voice_client:
+    await interaction.response.send_message('I\'m on break! Gimme something to play and I\'ll head to the bandstand')
     return
 
   songs.clear()
 
-async def play_next(ctx, voice):
+async def play_next(interaction: discord.Interaction):
+  if not band_bot.voice_client: return;
+
   if songs.empty():
-    await voice.disconnect()
+    await band_bot.disconnect()
     return
 
   song = songs.next()
@@ -137,20 +172,20 @@ async def play_next(ctx, voice):
     info = ydl.extract_info( song, download = False )
   title = info.get('title', 'Unknown') if info else 'Unknown'
 
-  await ctx.send(f'Next up: {title}')
-
   def after(error):
     if error:
       print(f'encountered error: {error}')
 
-    future = play_next(ctx, voice)
-    future = asyncio.run_coroutine_threadsafe(future, band_bot.loop)
+    future = play_next(interaction)
+    future = asyncio.run_coroutine_threadsafe(future, band_bot.bot.loop)
 
     try:
       future.result()
     except Exception as e:
       print(f'failed to play next song with exception: {e}')
 
-  voice.play(discord.FFmpegPCMAudio( song, options = '-vn' ), after)
 
-band_bot.run(DISC_TOKEN)
+  await interaction.response.send_message(f'Next up: {title}')
+  band_bot.voice_client.play(discord.FFmpegPCMAudio( song, options = '-vn' ), after = after)
+
+band_bot.bot.run(DISC_TOKEN)
