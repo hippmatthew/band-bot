a31bdea6c93d38c9324dbed40797317dd0794ec7
Matthew Hipp <hipp.matthew@icloud.com>
2025-08-02 00:43:47 +0000

    clearing old commmands

--- main.py
+++ main.py
@@ -1,31 +1,40 @@
 import asyncio
 import discord
 from discord.ext import commands
-from src.env import check_env, disc_token
+from collections import defaultdict
+from src.env import check_env, disc_token, guild_id
 
 _BAND_BOT: commands.Bot = commands.Bot(
-  command_prefix  = commands.when_mentioned_or('/'),
+  command_prefix  = commands.when_mentioned_or('!'),
   description     = 'The Jazz Lounge music bot',
   intents         = discord.Intents.all()
 )
 
 @_BAND_BOT.event
-async def on_ready() -> None:
+async def on_ready():
+  _BAND_BOT.tree.clear_commands()
+  print('commands cleared')
+
+  await _BAND_BOT.tree.sync()
+  print('commands synced')
+
   print(f'beating meat as {_BAND_BOT.user}')
 
 async def load_cogs() -> None:
   await _BAND_BOT.load_extension('src.cogs.music')
 
 def print_commands() -> None:
-  cogs: dict[str, list[str]] = {}
+  cogs: defaultdict[str, list[str]] = defaultdict(list)
   for cmd in _BAND_BOT.commands:
     cogs[cmd.cog_name or 'No Cog'].append(cmd.name)
 
   print('\n=== registered commands ===')
   for cog in cogs:
     print(f'\n[{cog}]')
-    for cmd in cog:
-      print(f'\t{cmd}')
+    for cmd in cogs[cog]:
+      print(f'* {cmd}')
+
+  print('')
 
 async def main() -> None:
   error_str: str = check_env()
